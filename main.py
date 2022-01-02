1414606d1d3d4dc8fd6933676c22027397534378
Aakash Singh <46787056+sainAk@users.noreply.github.com>
2022-01-02 19:34:22 +0530

    use different logic to get list of new posts

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python3
 
 import datetime
+import json
 from os import getenv
+from bs4 import BeautifulSoup as bs
 
 import requests
 import facebook_scraper
 
-cookies = {
+COOKIES = {
     "c_user": getenv("c_user"),
     "datr": getenv("datr"),
     "fr": getenv("fr"),
     "sb": getenv("sb"),
     "xs": getenv("xs"),
 }
+GROUP_ID = getenv("GROUP_ID")
+
+FB_BASE_URL = "https://mbasic.facebook.com"
 
 
 class TelegramBot:
@@ -20,43 +25,44 @@ class TelegramBot:
         self.bot_token = bot_token
         self.base_url = f"https://api.telegram.org/bot{bot_token}"
         self.chat_id = chat_id
+        self._payload = {
+            "chat_id": chat_id,
+            "parse_mode": "HTML",
+        }
+
+    def _make_request(self, method, payload):
+        response = requests.post(f"{self.base_url}/{method}", data=payload)
+        return response.json()
 
     def send_message(self, message):
         payload = {
-            "chat_id": self.chat_id,
             "text": message,
-            "parse_mode": "HTML",
+            **self._payload,
         }
-        response = requests.post(f"{self.base_url}/sendMessage", data=payload)
-        return response.json()
+        return self._make_request("sendMessage", payload)
 
     def send_photo(self, photo_url, message):
         payload = {
-            "chat_id": self.chat_id,
             "photo": photo_url,
             "caption": message,
-            "parse_mode": "HTML",
+            **self._payload,
         }
-        response = requests.post(f"{self.base_url}/sendPhoto", data=payload)
-        return response.json()
+        return self._make_request("sendPhoto", payload)
 
     def send_video(self, video_url, message):
         payload = {
-            "chat_id": self.chat_id,
             "video": video_url,
             "caption": message,
-            "parse_mode": "HTML",
+            **self._payload,
         }
-        response = requests.post(f"{self.base_url}/sendVideo", data=payload)
-        return response.json()
+        return self._make_request("sendVideo", payload)
 
     def send_media_group(self, media_entries):
         payload = {
             "chat_id": self.chat_id,
             "media": media_entries,
         }
-        response = requests.post(f"{self.base_url}/sendMediaGroup", data=payload)
-        return response.json()
+        return self._make_request("sendMediaGroup", payload)
 
 
 def format_post(post):
@@ -83,6 +89,21 @@ def format_post(post):
     return message
 
 
+def fetch_posts(group_id):
+    session = requests.Session()
+    session.cookies = requests.utils.cookiejar_from_dict(COOKIES)
+    response = session.get(f"{FB_BASE_URL}/groups/{group_id}")
+    if not response.ok:
+        return []
+    soup = bs(response.text, "lxml")
+    posts = soup.select("#m_group_stories_container>div>div")
+    post_ids = []
+    for post in posts:
+        post_data_ft = json.loads(post.get("data-ft"))
+        post_ids.append(post_data_ft["top_level_post_id"])
+    return post_ids
+
+
 if __name__ == "__main__":
 
     bot = TelegramBot(
@@ -90,36 +111,31 @@ if __name__ == "__main__":
         chat_id=getenv("TELEGRAM_CHAT_ID"),
     )
 
-    time_offset = (
-        datetime.datetime.utcnow()
-        - datetime.timedelta(hours=int(getenv("TIME_OFFSET", 3)), minutes=2)
-    ).timestamp()
+    try:
+        with open("session.txt", "r") as f:
+            old_post_ids = set(f.read().splitlines())
+    except (FileNotFoundError, TypeError) as e:
+        print(e)
+        old_post_ids = set()
+
+    all_post_ids = set(fetch_posts(getenv("GROUP_ID"))) | old_post_ids
+    new_post_ids = all_post_ids - old_post_ids
+
+    new_post_urls = [
+        f"{FB_BASE_URL}/groups/{GROUP_ID}/permalink/{post_id}" for post_id in new_post_ids
+    ]
 
     # facebook_scraper.enable_logging()
     try:
         _posts = facebook_scraper.get_posts(
-            group=getenv("GROUP_ID"), cookies=cookies, options={"progress": True}
+            cookies=COOKIES,
+            post_urls=new_post_urls,
         )
     except facebook_scraper.exceptions.LoginError:
         bot.send_message("Login failed. Someone stole your cookies.")
         exit(1)
 
-    try:
-        with open("session.txt", "r") as f:
-            last_post_id = f.read().strip()
-            if last_post_id == "None":
-                raise TypeError("last_post_id was none")
-    except (FileNotFoundError, TypeError) as e:
-        print(e)
-        last_post_id = None
-
-    posts = []
-    for post in _posts:
-        if last_post_id and last_post_id == str(post["post_id"]):
-            break
-        posts.append(post)
-
-    for post in posts[::-1]:
+    for post in list(_posts)[::-1]:
         try:
             if post["image"]:
                 if len(post["images"]) == 1:
@@ -143,4 +159,4 @@ if __name__ == "__main__":
             continue
 
     with open("session.txt", "w+") as f:
-        f.write(str(last_post_id))
+        f.write("\n".join(all_post_ids))
